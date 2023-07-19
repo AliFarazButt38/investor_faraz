a6b88a32b3cec152f66e4981a02eaf51dc2eaf2c
Shargeel Ahmed <72449945+shargeel071@users.noreply.github.com>
2023-07-19 17:31:25 +0500

    Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -2,53 +2,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:investor_flutter/Theme/theme_constant.dart';
 import 'package:investor_flutter/Theme/theme_manager.dart';
-import 'package:investor_flutter/View/Screen/bank_Screens/transaction_screen1.dart';
-import 'package:investor_flutter/View/Screen/bank_Screens/transactions2_screen.dart';
-import 'package:investor_flutter/View/Screen/bottom_navigation/bottom_navigation.dart';
-import 'package:investor_flutter/View/Screen/complete_your_profile/account_typeScreen.dart';
-import 'package:investor_flutter/View/Screen/complete_your_profile/complete_profileScreen.dart';
-import 'package:investor_flutter/View/Screen/complete_your_profile/employement_InfoScreen.dart';
-import 'package:investor_flutter/View/Screen/complete_your_profile/enterprise_infoScreen.dart';
-import 'package:investor_flutter/View/Screen/complete_your_profile/fund_accountScreen.dart';
-import 'package:investor_flutter/View/Screen/complete_your_profile/personal_informationScreen.dart';
-import 'package:investor_flutter/View/Screen/complete_your_profile/statements_checkScreen.dart';
-import 'package:investor_flutter/View/Screen/complete_your_profile/upload_documentsScreen.dart';
-import 'package:investor_flutter/View/Screen/emailAndPhone/choose_password_screen.dart';
 import 'package:investor_flutter/View/Screen/emailAndPhone/email_address_screen.dart';
-import 'package:investor_flutter/View/Screen/emailAndPhone/email_verificationScreen.dart';
-import 'package:investor_flutter/View/Screen/emailAndPhone/email_verifiedScreen.dart';
-import 'package:investor_flutter/View/Screen/emailAndPhone/login_accountScreen.dart';
-import 'package:investor_flutter/View/Screen/emailAndPhone/phone_otpScreen.dart';
-import 'package:investor_flutter/View/Screen/home_screens/dashboard_screen.dart';
-import 'package:investor_flutter/View/Screen/home_screens/explore_investmentScreen.dart';
-import 'package:investor_flutter/View/Screen/home_screens/home_screen.dart';
-import 'package:investor_flutter/View/Screen/home_screens/investmentInfo_screen.dart';
-import 'package:investor_flutter/View/Screen/home_screens/property_infoScreen.dart';
-import 'package:investor_flutter/View/Screen/home_screens/wealth_calculatorScreen.dart';
-import 'package:investor_flutter/View/Screen/invest_ContributionScreen/investContribution_screen1.dart';
-import 'package:investor_flutter/View/Screen/invest_ContributionScreen/investContribution_screen2.dart';
-import 'package:investor_flutter/View/Screen/onBoarding/onboarding_screen.dart';
-import 'package:investor_flutter/View/Screen/profile_screens/personal_profile_infoScreen.dart';
-import 'package:investor_flutter/View/Screen/profile_screens/profile_screen.dart';
-import 'package:investor_flutter/View/Screen/profile_screens/update_profileScreen.dart';
-import 'package:investor_flutter/View/Screen/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'View/Screen/complete_your_profile/document_Screen.dart';
+import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isDarkMode = prefs.getBool('isDarkMode') ?? false;
-
 
   runApp(ChangeNotifierProvider<ThemeManager>(
       create:(_)=>ThemeManager(isDarkMode),
       child:  MyApp()));
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
